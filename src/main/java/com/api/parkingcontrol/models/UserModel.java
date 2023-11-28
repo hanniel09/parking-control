d4296fda06478d48b11d0571602d0bf8e2a0c40b
hanniel09 <hannielvieira1227@gmail.com>
2023-11-28 13:25:54 -0300

    feat: update user model

--- src/main/java/com/api/parkingcontrol/models/UserModel.java
+++ src/main/java/com/api/parkingcontrol/models/UserModel.java
@@ -1,13 +1,16 @@
 package com.api.parkingcontrol.models;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.UUID;
 
 @Entity
 @Table(name = "TB_USER")
-public class UserModel implements Serializable {
+public class UserModel implements UserDetails, Serializable {
     private static final long serialVersionUUID = 1L;
 
     @Id
@@ -19,4 +22,55 @@ public class UserModel implements Serializable {
 
     @Column(nullable = false)
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
