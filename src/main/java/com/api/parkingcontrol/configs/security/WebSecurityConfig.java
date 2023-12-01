d5df98621cf7186725f3cd8f82a2facff6047bbe
hanniel09 <hannielvieira1227@gmail.com>
2023-12-01 17:18:32 -0300

    feat:update WebSecurityConfig

--- src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.api.parkingcontrol.configs.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,6 +18,9 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.GET, "/parking-spot/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/parking-spot" ).hasRole("USER")
+                        .requestMatchers(HttpMethod.DELETE, "/parking-spot/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
